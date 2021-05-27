695956f08f3b26838300c093f4ce67687e9e44cf
Murilo Esteca Arelhano <51941771+MuriloArelhano@users.noreply.github.com>
2021-05-26 22:49:53 -0400

    Importando múltiplos arquivos de uma vez
    Para importar múltiplos arquivos de uma vez, os arquivos tem que seguir o padrão implantado nas pastas

--- coleta_transformacao_dados.py
+++ coleta_transformacao_dados.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-cities = []
+cities = ['fortaleza', 'rio_de_janeiro', 'curitiba']
 
 
 # %%
@@ -14,45 +14,66 @@ def normalize_date(data, column_date_name):
 
 
 # %%
-df_dengue = pd.read_csv('./info_dengue.mat.csv', delimiter=',')
-df_dengue = df_dengue.drop(columns=['casos_est', 'casos_est_min', 'casos_est_max', 'SE', 'Localidade_id', 'receptivo',	'transmissao',
-                                    'nivel_inc', 'notif_accum_year', 'p_rt1', 'p_inc100k', 'nivel', 'id', 'versao_modelo', 'tweet', 'Rt', 'tempmin', 'umidmax'])
+def transformDengueData(citiesNameArray):
+    df_dengue_list = []
 
-df_dengue = normalize_date(df_dengue, 'data_iniSE')
+    for city in citiesNameArray:
+        df_dengue = pd.read_csv(
+            f'./datasets_dengue/{city}_dengue.csv', delimiter=',')
+        df_dengue['cidade'] = city
+        df_dengue_list.append(df_dengue)
+
+    df_dengue_full = pd.concat(df_dengue_list, axis=0, ignore_index=True)
+    df_dengue_full = df_dengue_full.drop(columns=['casos_est', 'casos_est_min', 'casos_est_max', 'SE', 'Localidade_id', 'receptivo',	'transmissao',
+                                                  'nivel_inc', 'notif_accum_year', 'p_rt1', 'p_inc100k', 'nivel', 'id', 'versao_modelo', 'tweet', 'Rt', 'tempmin', 'umidmax'])
+    df_dengue_full = normalize_date(df_dengue_full, 'data_iniSE')
+    df_dengue_full = df_dengue_full.groupby(['cidade', 'data_iniSE'], as_index=False).agg({
+        'casos': sum, 'pop': min})
+
+    return df_dengue_full
+
+
+df_dengue = transformDengueData(cities)
 print(df_dengue)
 
 
 # %%
-df_dengue = df_dengue.groupby('data_iniSE', as_index=False).agg({
-    'casos': sum, 'pop': 'mean'})
-print(df_dengue)
+
+def transformClimaData(citiesNameArray):
+    df_clima_list = []
+    for city in citiesNameArray:
+        df_clima = pd.read_csv(
+            f'./datasets_clima/{city}_clima.csv', delimiter=';')
+        df_clima = df_clima.iloc[:, :-1]
+        df_clima['cidade'] = city
+        df_clima_list.append(df_clima)
+
+    df_clima_full = pd.concat(df_clima_list, axis=0, ignore_index=True)
+    df_clima_full = df_clima_full.dropna()
+
+    df_clima_full.columns = df_clima_full.columns.str.replace('[#,@,&]', '')
+    df_clima_full.columns = df_clima_full.columns.str.replace(' ', '_')
+    df_clima_full.columns = map(str.lower, df_clima_full.columns)
+
+    df_clima_full = normalize_date(df_clima_full, 'data_medicao')
+    return df_clima_full
 
 
-# %%
-df_clima = pd.read_csv('./dados_clima.csv', delimiter=';')
-df_clima = df_clima.iloc[:, :-1]
-df_clima = df_clima.dropna()
-
-df_clima.columns = df_clima.columns.str.replace('[#,@,&]', '')
-df_clima.columns = df_clima.columns.str.replace(' ', '_')
-df_clima.columns = map(str.lower, df_clima.columns)
-
-df_clima = normalize_date(df_clima, 'data_medicao')
-
-
+df_clima = transformClimaData(cities)
 print(df_clima)
 
 
 # %%
 
-df_final = pd.merge(df_clima, df_dengue, left_on='data_medicao',
-                    right_on='data_iniSE', how='inner')
+df_final = pd.merge(df_clima, df_dengue, left_on=['data_medicao', 'cidade'],
+                    right_on=['data_iniSE', 'cidade'], how='inner', suffixes=('_y', '_x'))
 df_final = df_final.drop(columns=['data_iniSE'])
-df_final = df_final.set_index('data_medicao')
+df_final = df_final.sort_values(by=['data_medicao'])
 print(df_final)
 
 
 # %%
-plt.plot(df_final['data_medicao'], df_final['casos'])
+df_final_graph = df_final.pivot(index='data_medicao', columns='cidade', values='casos')
+df_final_graph.plot()
 
 # %%
