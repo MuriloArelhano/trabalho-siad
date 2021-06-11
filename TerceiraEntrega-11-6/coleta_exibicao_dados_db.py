284ba9979a5d8c80a5206f1b1f2e6014aa8eec29
Murilo Esteca Arelhano <51941771+MuriloArelhano@users.noreply.github.com>
2021-06-11 13:41:33 -0400

    Finalizando terceira entrega

--- TerceiraEntrega-11-6/coleta_exibicao_dados_db.py
+++ TerceiraEntrega-11-6/coleta_exibicao_dados_db.py
@@ -15,7 +15,7 @@ try:
                         user='postgres', password='teste',
                         host='localhost', port='5433')
     cursor = conn.cursor()
-    db_dataframe = pd.read_sql_query("""select data.mes, data.ano, cidade.nome as cidade, cidade.pop, cidade.estado, dengue.n_casos, clima.temp_max_celsius, clima.temp_med_celsius, clima.temp_min_celsius, clima.umidade_relativa, clima.precipitacao_mensal_mm
+    db_dataframe = pd.read_sql_query("""select data.mes, data.ano, cidade.nome as cidade, cidade.estado, dengue.n_casos, clima.temp_max_celsius, clima.temp_med_celsius, clima.temp_min_celsius, clima.umidade_relativa, clima.precipitacao_mensal_mm
         from dengue
         inner join clima on clima.id = dengue.clima_id
         inner join data on data.id = dengue.data_id
@@ -61,28 +61,19 @@ final_dataframe['precipitacao_mensal_mm_norm'] = precipitacao_mensal_mm_norm
 
 final_dataframe['temp_med_celsius_norm'] = temp_med_celsius_norm
 
-final_dataframe['pop_casos_percent'] = (
-    db_dataframe['n_casos']*100)/db_dataframe['pop']
-final_dataframe
+final_dataframe['pop_infectada_percent'] = (db_dataframe['n_casos']*100)/db_dataframe['n_casos']
 
 # %%
 db_dataframe_graph = db_dataframe.pivot(
     index='data', columns='cidade', values='n_casos')
-db_dataframe_graph.plot(figsize=(15, 7), title='Numero de casos por cidade', xlim=[
+db_dataframe_graph.plot(title='Numero mensal de casos por cidade ',figsize=(15, 7), xlim=[
     datetime.date(2012, 1, 1), datetime.date(2016, 2, 1)], style='-o')
-
-#%%
-db_dataframe_graph = final_dataframe.pivot(
-    index='data', columns='cidade', values=['pop_casos_percent'])
-db_dataframe_graph.plot(figsize=(15, 7), title='Porcentagem de infectados pela população', xlim=[
-    datetime.date(2012, 1, 1), datetime.date(2016, 2, 1)], style='-o')
-
 
 # %%
 final_dataframe_graph = final_dataframe.pivot(index='data', columns='cidade', values=[
     'n_casos_norm', 'precipitacao_mensal_mm_norm'])
 
-final_dataframe_graph.plot(figsize=(15, 7), title="Relação crescimento dos casos com precipitação", xlim=[
+final_dataframe_graph.plot(figsize=(15, 7), title="Relação crescimento dos casos com precipitação (Rio de Janeiro)", xlim=[
     datetime.date(2012, 1, 1), datetime.date(2016, 2, 1)], style='-o')
 
 
@@ -90,7 +81,7 @@ final_dataframe_graph.plot(figsize=(15, 7), title="Relação crescimento dos cas
 final_dataframe_graph = final_dataframe.pivot(index='data', columns='cidade', values=[
     'n_casos_norm', 'temp_med_celsius_norm'])
 
-final_dataframe_graph.plot(figsize=(15, 7), title="Relação crescimento dos casos temperatura média", xlim=[
+final_dataframe_graph.plot(figsize=(15, 7), title="Relação crescimento dos casos temperatura média (Rio de Janeiro)", xlim=[
                            datetime.date(2012, 1, 1), datetime.date(2016, 5, 1)], style='-o')
 
 # %%
